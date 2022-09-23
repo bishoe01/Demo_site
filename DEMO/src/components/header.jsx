aaec9d552c5a48465cb2116104f06b939a99ec87
bishoe01 <bishoe01@gmail.com>
2022-09-23 16:13:47 +0900

    noto sans 폰트 적용 및 navitem select css

--- DEMO/src/components/header.jsx
+++ DEMO/src/components/header.jsx
@@ -1,28 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import "../App.scss";
 function Header(props) {
     const navigate = useNavigate();
-    const [clickItem, setClickItem] = useState(false);
-    const OnSelect = (e) => {
-        let item__name = e.target.innerHTML;
-        if (item__name === "Home") {
-            window.location.href = "/";
-        }
-    }
+    const [clickItem, setClickItem] = useState("home");
+    
     return (
         <section className='navbar'>
             <img className='navbar__logo' src="https://drx-media.s3.ap-northeast-2.amazonaws.com/image/icon/icon-no-padding.png" alt="logoImg" />
             <div className='navbar__items'>
-                    <Link to="/" className='navbar__item' onClick={(
-                        e => {
-                            setClickItem(true);
-                            console.log(e.target.innerHTML);
-                        }
-                    )} >ABOUT</Link>
-                    <Link to="/teams" className='navbar__item'>TEAMS</Link>
-                    <Link to="/partners" className='navbar__item'>PARTNERS</Link>
-                    <Link to="/schedule" className='navbar__item'>SCHEDULE</Link>
+                <Link to="/" className='navbar__item' onClick={(e) => setClickItem(e.target.innerHTML)}
+                style={{
+                    borderBottom: clickItem === "HOME" ? "3px solid #5A8DFF" : "",
+                    fontWeight : clickItem === "HOME" ? "bold" : ""
+                }}>HOME</Link>
+                <Link to="/teams" className='navbar__item' onClick={(e) => setClickItem(e.target.innerHTML)}
+                style={{
+                    borderBottom: clickItem === "TEAMS" ? "3px solid #5A8DFF" : "",
+                    fontWeight : clickItem === "TEAMS" ? "bold" : ""
+                }}>TEAMS</Link>
+                <Link to="/partners" className='navbar__item' onClick={(e) => setClickItem(e.target.innerHTML)}
+                style={{
+                    borderBottom: clickItem === "PARTNERS" ? "3px solid #5A8DFF" : "",
+                    fontWeight : clickItem === "PARTNERS" ? "bold" : ""
+                }}
+                >PARTNERS</Link>
+                <Link to="/schedule" className='navbar__item' onClick={(e)=>setClickItem(e.target.innerHTML)} 
+                style={{
+                    borderBottom: clickItem === "SCHEDULE" ? "3px solid #5A8DFF" : "",
+                    fontWeight : clickItem === "SCHEDULE" ? "bold" : ""
+                }}
+                >SCHEDULE</Link>
             </div>
         </section>
     );
