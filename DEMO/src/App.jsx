ff93c26fd97858df6672b78720b3eb9fa9dca920
bishoe01 <bishoe01@gmail.com>
2022-09-27 23:19:32 +0900

    logo banner

--- DEMO/src/App.jsx
+++ DEMO/src/App.jsx
@@ -3,11 +3,13 @@ import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import { ButtonDefault, ButtonPrimary } from './components/styledComponets/Default';
 import Header from './components/header';
+import Homepage from './components/homepage';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Homepage/>
       <Routes>
         <Route path="/" element={<></>} />
         <Route path="/profile" element={<h1>profile</h1>} />
---
+++ DEMO/src/components/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../App.scss";
+function Homepage(props) {
+    return (
+        <div className='home__banner'>
+        <img className='home__img' src='./img/DRX_Logo.png'/>
+            <p className='title'>WE ARE DRX</p>
+            </div>
+        
+    );
+}
+
+export default Homepage;
