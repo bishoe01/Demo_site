7aa56a5b3fc0720e5858ef8b3f004b3c31b2e6bc
bishoe01 <bishoe01@gmail.com>
2022-10-05 22:55:42 +0900

    typeit animation

--- DEMO/src/components/homepage.jsx
+++ DEMO/src/components/homepage.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TypeIt from "typeit-react";
 import "../App.scss";
 function Homepage(props) {
+    const [buttonText, setButtonText] = useState("Freeze");
+    const [instance, setInstance] = useState(null);
+    const toggleFreeze = () => {
+        if (instance.is("frozen")) {
+            instance.unfreeze();
+            setButtonText("Freeze");
+            return;
+        }
+
+        instance.freeze();
+        setButtonText("Unfreeze");
+    };
     return (
         <div className='home__banner'>
-        <img className='home__img' src='./img/DRX_Logo.png'/>
-            </div>
-        
+            <button onClick={toggleFreeze}>{buttonText}</button>
+
+            <TypeIt
+                options={{ loop: false }}
+                getAfterInit={(instance) => {
+                    setInstance(instance);
+                    return instance;
+                }}
+            >
+                WE ARE DRX
+            </TypeIt>
+        </div>
+
     );
 }
 
